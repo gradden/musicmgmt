e801eeff9fdbfb7a00abaf7c75e0221ece02bc32
Tóth Gergő <tgery3@gmail.com>
2023-11-25 15:09:49 +0100

    Install backpack

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,4 @@
 <?php
-
-use App\Http\Controllers\AuthController;
-use App\Http\Middleware\RedirectIfAuthenticated;
-use App\Livewire\Clubs;
-use App\Livewire\Concerts;
-use App\Livewire\Dashboard;
-use App\Livewire\Login;
-use App\Livewire\Profile;
-use Illuminate\Support\Facades\Route;
-
-use function Termwind\style;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,23 +9,3 @@ use function Termwind\style;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::middleware(RedirectIfAuthenticated::class)->group(function() {
-    Route::get('login', Login::class)->name('login');
-});
-Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])
-    ->middleware(['auth', 'signed'])
-    ->name('verification.verify');
-
-Route::post('/email/verification-notification', [AuthController::class, 'resendEmail'])
-    ->middleware(['auth', 'throttle:6,1'])
-    ->name('verification.send');
-
-Route::group(['middleware' => ['auth', 'verified']], function () {
-    Route::get('', Dashboard::class)->name('home');
-    Route::get('profile', Profile::class)->name('profile');
-    Route::get('concerts', Concerts::class)->name('concerts');
-    Route::get('concerts/{id}', Concerts::class)->name('concerts.show');
-    Route::get('clubs', Clubs::class)->name('clubs');
-    Route::get('clubs/{id}', [Clubs::class, 'show'])->name('clubs.show');
-});
